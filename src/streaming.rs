0d75d3db940e9f2063ff59661269031f0ced3139
Misha Zharov <mishazharov1@gmail.com>
2023-05-26 00:02:14 -0700

    Add streaming impl of quoted-string

--- src/streaming.rs
+++ src/streaming.rs
@@ -1,11 +1,17 @@
 use std::ops::{RangeFrom, RangeTo};
 
 use nom::{
-    character::streaming::satisfy, combinator::recognize, error::ParseError, multi::many1_count,
+    branch::alt,
+    character::streaming::char,
+    character::streaming::satisfy,
+    combinator::recognize,
+    error::ParseError,
+    multi::{fold_many0, many1_count},
+    sequence::{delimited, preceded},
     AsChar, IResult, InputIter, InputLength, Offset, Slice,
 };
 
-use crate::is_tchar;
+use crate::{is_qdtext, is_quoted_pair, is_tchar};
 
 /// TCHAR = "!" / "#" / "$" / "%" / "&" / "'" / "*"
 ///       / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
@@ -29,11 +35,52 @@ where
     recognize(many1_count(tchar))(input)
 }
 
+/// QDTEXT = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+pub fn qdtext<I, E>(input: I) -> IResult<I, char, E>
+where
+    I: InputIter + Slice<RangeFrom<usize>> + Copy,
+    <I as InputIter>::Item: AsChar,
+    E: ParseError<I>,
+{
+    satisfy(is_qdtext)(input)
+}
+
+/// QUOTED-PAIR = "\" ( HTAB / SP / VCHAR / obs-text )
+fn quoted_pair<I, E>(input: I) -> IResult<I, char, E>
+where
+    I: InputIter + Slice<RangeFrom<usize>> + Copy + InputLength,
+    <I as InputIter>::Item: AsChar,
+    E: ParseError<I>,
+{
+    preceded(char('\\'), satisfy(is_quoted_pair))(input)
+}
+
+/// QUOTED-STRING = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+pub fn quoted_string<I, E>(input: I) -> IResult<I, String, E>
+where
+    I: InputIter + Slice<RangeFrom<usize>> + Slice<RangeTo<usize>> + Copy + InputLength + Offset,
+    <I as InputIter>::Item: AsChar,
+    E: ParseError<I>,
+{
+    delimited(
+        char('"'),
+        fold_many0(
+            alt((qdtext, quoted_pair)),
+            String::default,
+            |mut collection, input| {
+                collection.push(input);
+                collection
+            },
+        ),
+        char('"'),
+    )(input)
+}
+
 #[cfg(test)]
 mod tests {
     use nom::{error::VerboseError, Err as OutCome, Needed};
 
-    use crate::streaming::{tchar, token};
+    use crate::streaming::{quoted_string, tchar, token};
 
     #[test]
     fn test_tchar() {
@@ -64,5 +111,38 @@ mod tests {
             token::<_, VerboseError<&str>>(","),
             Err(OutCome::Error(_))
         ));
+    }
+
+    #[test]
+    fn test_quoted_string() {
+        assert_eq!(
+            quoted_string::<_, VerboseError<&str>>(r#""""#),
+            Ok(("", String::from(r#""#)))
+        );
+
+        assert_eq!(
+            quoted_string::<_, VerboseError<&str>>(r#""hello""#),
+            Ok(("", String::from(r#"hello"#)))
+        );
+
+        assert_eq!(
+            quoted_string::<_, VerboseError<&str>>(r#""\"hello""#),
+            Ok(("", String::from(r#""hello"#)))
+        );
+
+        assert!(matches!(
+            quoted_string::<_, VerboseError<&str>>(r#""awd"#),
+            Err(OutCome::Incomplete(Needed::Unknown))
+        ));
+
+        assert!(matches!(
+            quoted_string::<_, VerboseError<&str>>(r#" "text""#),
+            Err(OutCome::Error(_))
+        ));
+
+        assert_eq!(
+            quoted_string::<_, VerboseError<&str>>(r#""awd"trailing"#),
+            Ok(("trailing", String::from("awd")))
+        );
     }
 }
